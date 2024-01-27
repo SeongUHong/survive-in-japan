cce8721ffaab838c11ee4c9d02b22d52250bdc1f
SeongUHong <swhong369@gmail.com>
2024-01-27 08:31:35 +0000

    vue.js 도입

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Laravel + Vue Test</h1>
+        <example-component></example-component>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello from Vue!'
+        };
+    }
+}
+</script>
+
+<style scoped>
+/* Your component styles here */
+</style>
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'This is a Vue component!'
+        };
+    }
+}
+</script>
+
+<style scoped>
+/* Your component styles here */
+</style>
