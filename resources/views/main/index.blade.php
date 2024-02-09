c737dc07c266a91e34362ff18ac18b88efac90b6
SeongUHong <swhong369@gmail.com>
2024-02-09 14:38:58 +0000

    scss도입

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('/css/main/app.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/pages/main.css') }}">
     <title>TOP</title>
   </head>
   <body class="base-bg-color">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
 const mix = require('laravel-mix');
 
+// JS
 mix.js('resources/js/pages/admin_write.js', 'public/js/pages')
    .version();
+
+// SCSS
+mix.sass('resources/sass/pages/main.scss', 'public/css/pages').options({
+   processCssUrls: false, // CSS내의 URL을 변경하지 않음
+});
