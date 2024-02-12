7f99fd777d7bd01d83e5faf4ec71450bc9e9a0e8
SeongUHong <swhong369@gmail.com>
2024-02-12 12:32:27 +0000

    updated content card css

--- resources/views/main/_sub/content_card.blade.php
+++ resources/views/main/_sub/content_card.blade.php
@@ -1,15 +1,10 @@
-<div class="main-middle-content row">
+<div class="main-middle-content row" onclick="location.href='{{ url('post_view').'/'.$post['id'] }}';">
   <div class="main-middle-content-thumb col-3">
     <img src="{{ '/' . \App\Consts\Image::READ_BASE_DIRECTORY . $post['thumb_path']}}">
   </div>
   <div class="main-middle-content-text col-9">
     <div class="main-middle-content-text-title fs-5 fw-bold">
-      <a href="{{ url('post_view').'/'.$post['id'] }}">
-        {{ $post['title'] }}
-      </a>
-    </div>
-    <div class="main-middle-content-text-detail fs-6">
-      {{ $post['content'] }}
+      {{ $post['title'] }}
     </div>
   </div>
 </div>
