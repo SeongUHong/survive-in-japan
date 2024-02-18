9bfbb578d69203c4bbf26b8a24debd8af8850bb4
SeongUHong <swhong369@gmail.com>
2024-02-18 09:01:46 +0000

    updated content card css

--- resources/views/main/_sub/small_content_card.blade.php
+++ resources/views/main/_sub/small_content_card.blade.php
@@ -1,8 +1,8 @@
 <div class="main-middle-small-content row" data-post-id="{{ $post['id'] }}">
-  <div class="main-middle-small-content-thumb col-3">
+  <div class="main-middle-small-content-thumb col-4">
     <img src="{{ '/' . \App\Consts\Image::READ_BASE_DIRECTORY . $post['thumb_path']}}">
   </div>
-  <div class="main-middle-small-content-text col-9">
+  <div class="main-middle-small-content-text col-8">
     <div class="main-middle-small-content-text-title">
       {{ $post['title'] }}
     </div>
